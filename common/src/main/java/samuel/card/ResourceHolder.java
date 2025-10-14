6843b0fd93cbe0cd4910e1674832758ee17b916e
Samuels Desktop <cool23man23@gmail.com>
2025-10-14 11:15:52 +0200

    liskov

--- common/src/main/java/samuel/card/ResourceHolder.java
+++ common/src/main/java/samuel/card/ResourceHolder.java
@@ -4,4 +4,17 @@ import samuel.resource.ResourceAmount;
 
 public interface ResourceHolder {
     ResourceAmount getResourceAmount();
+    /**
+     * Adds to the resource pile.
+     * @param amount
+     * @return The amount still left to add after reaching max.
+     */
+    int increaseResource(int amount);
+
+    /**
+     * Removes from the resource pile
+     * @param amount
+     * @return The amount still left to remove.
+     */
+    int decreaseAmount(int amount);
 }
--- common/src/main/java/samuel/card/region/RegionCard.java
+++ common/src/main/java/samuel/card/region/RegionCard.java
@@ -7,19 +7,7 @@ import samuel.resource.ResourceAmount;
 
 public interface RegionCard extends PlaceableCard, ResourceHolder {
 
-    /**
-     * Adds to the resource pile.
-     * @param amount
-     * @return The amount still left to add after reaching max.
-     */
-    int increaseResource(int amount);
 
-    /**
-     * Removes from the resource pile
-     * @param amount
-     * @return The amount still left to remove.
-     */
-    int decreaseAmount(int amount);
 
     /**
      * Returns the dice roll associated with the region. E.g. the number that the production die need to roll to yield resources.
