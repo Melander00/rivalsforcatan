cce61be46180545893b57ee95e0ebd35c9df868e
Samuels Desktop <cool23man23@gmail.com>
2025-10-15 15:28:33 +0200

    todos 2

--- common/src/main/java/samuel/card/event/InventionEventCard.java
+++ common/src/main/java/samuel/card/event/InventionEventCard.java
@@ -5,6 +5,10 @@ import samuel.event.card.InventionEvent;
 import samuel.game.GameContext;
 import samuel.player.Player;
 import samuel.card.CardID;
+import samuel.player.request.RequestCause;
+import samuel.player.request.RequestCauseEnum;
+import samuel.point.points.ProgressPoint;
+import samuel.resource.ResourceBundle;
 
 import java.util.List;
 import java.util.UUID;
@@ -34,7 +38,15 @@ public class InventionEventCard implements EventCard {
         context.getEventBus().fireEvent(event);
         if(event.isCanceled()) return;
 
-        List<Player> players = context.getPlayers(); // todo
+        List<Player> players = context.getPlayers();
+        for(Player player : players) {
+            int times = player.getPoints(ProgressPoint.class);
+            while(times > 0) {
+                ResourceBundle bundle = player.requestResource(ResourceBundle.oneOfAll(), 1, new RequestCause(RequestCauseEnum.FREE_RESOURCE));
+                player.giveResources(bundle);
+                times--;
+            }
+        }
 
         context.getEventBus().fireEvent(new InventionEvent.Post());
     }
