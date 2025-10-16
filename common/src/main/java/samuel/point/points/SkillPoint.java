c718df6bee60509b25d2b0f1326afbe2e403f58f
Samuels Desktop <cool23man23@gmail.com>
2025-10-16 12:27:17 +0200

    added strength and skill points

---
+++ common/src/main/java/samuel/point/points/SkillPoint.java
@@ -0,0 +1,11 @@
+package samuel.point.points;
+
+import samuel.point.Point;
+
+public class SkillPoint implements Point {
+
+    @Override
+    public String getName() {
+        return "Skill Point";
+    }
+}
---
+++ common/src/main/java/samuel/point/points/StrengthPoint.java
@@ -0,0 +1,11 @@
+package samuel.point.points;
+
+import samuel.point.Point;
+
+public class StrengthPoint implements Point {
+
+    @Override
+    public String getName() {
+        return "Strength Point";
+    }
+}
